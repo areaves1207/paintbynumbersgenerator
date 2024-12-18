5ae41b1f2d517a8d59567223eaaab52ba9eb19e3
areaves1207 <areaves0816@gmail.com>
2024-12-18 02:41:51 -0700

    Added hysteresis file.

---
+++ hysteresis.py
@@ -0,0 +1,28 @@
+#File for hysteresis tracking
+import numpy as np
+
+def hysteresis(strong_edges, weak_edges):
+    rows, cols = strong_edges.shape
+    result = strong_edges #must be init to strong bc we are only adding "weak" pixels to strong.
+    
+    isStrong = False
+    
+    thresh = 3
+    
+    for i in range(thresh, rows-thresh):
+        for j in range(thresh, cols-thresh):
+            
+            if(weak_edges[i][j] != 0):
+                #check if any of the surrounding 8 pixels are strong
+                for x in range(-thresh, thresh+1):
+                    for y in range(-thresh, thresh+1):
+                        if(strong_edges[i+x][j+y] != 0):
+                            result[i][j] = 255
+                            isStrong = True
+                            
+                if(not isStrong):
+                    result[i][j] = 0
+                isStrong = False
+                
+
+    return result
