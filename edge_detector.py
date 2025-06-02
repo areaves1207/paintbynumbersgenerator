74b1db69330a5f6b852f5ba44bc6273a0cf2983b
areaves12 <areaves@mines.edu>
2025-06-03 01:52:36 +0200

    Minor changes again.

--- edge_detector.py
+++ edge_detector.py
@@ -20,12 +20,12 @@ def display_image(img, title="figure"):
 # Track edge by hysteresis: Finalize the detection of edges by suppressing all the other edges that are weak and not connected to strong edges.
 # def detect_edges(image_name=""):
 #0) Import image and turn into greyscale:
-img = cv.imread("test_images/woman_in_hallway.png", 0) #Read in file as greyscale img
+img = cv.imread("test_images/1920x1080.jpg", 0) #Read in file as greyscale img
 # display_image(img)
 
 #1) Apply gaussian filters to x and y
-size = 5
-sigma = 2 #TODO: play around with these numbers to see what works best and not. 5,2 seems to be best
+size = 25
+sigma = 5 #TODO: play around with these numbers to see what works best and not. 5,2 seems to be best
 
 gaussian = GaussianImage(size, sigma, img)
 # gaussian.show_filtered_images()
@@ -47,13 +47,13 @@ thresh, strong_edges, weak_edges = thresh(non_max_image, 50, 150) #TODO: has 2 o
 hyst = hysteresis(strong_edges, weak_edges)
 
 #Connect patches
-final = cv.morphologyEx(hyst, cv.MORPH_GRADIENT, np.ones((3, 3), np.uint8))
+# final = cv.morphologyEx(hyst, cv.MORPH_GRADIENT, np.ones((3, 3), np.uint8))
 
 
 #DISPLAY STEPS
 # display_image(strong_edges,  "Strong edges")
 # display_image(weak_edges, "Weak edges")
 # display_image(thresh, "Threshing")
-# display_image(hyst, "Hystersis Completed")
-display_image(final, "Connected Patches")
+display_image(hyst, "Hystersis Completed")
+# xdisplay_image(final, "Connected Patches")
 
