f9c256533a9f80da41453bfd47a230b90a450245
areaves1207 <areaves0816@gmail.com>
2025-06-04 00:53:27 -0600

    Minor changes

--- color_clustering.py
+++ color_clustering.py
@@ -3,19 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from gaussian import GaussianImage
 
-img = cv.imread("test_images/1920x1080.jpg", -1) #Read in file as is
-img_height, img_width, color = img.shape
-
-size = 15
-sigma = 5 #TODO: play around with these numbers to see what works best and not. 5,2 seems to be best
-gaussian = GaussianImage(size, sigma, img) #prob want a bi-soemthing blur too. preserves edges better
-
-num_colors = 128
-
 
 def k_means_clustering(k, img):
     # flat_img = img.reshape(img_height, img_width, 3) #we only need this if the img isnt formatted right. (x,y,BGR)
-    centers = pick_centers(k, img)
+    # centers = pick_centers(k, img) #now we have k (x,y,z)
+    
+    img_copy = [subarray + [-1] for subarray in img]
+    print(img_copy[0])
+    #Assign colors to clusters
+    
 
 
 
@@ -24,14 +20,16 @@ def k_means_clustering(k, img):
 
     
 def pick_centers(k, img): #generates k random center points that lie within the RGB space
-    height, width, pixels = img.shape
+    height, width = img.shape
+    print(img)
     #pick k random centers on our image to start
-    centers = {}
-    temp_coords = {} #holds random x and y bounded within our pixel array size
+    centers = []
+    temp_coords = set() #holds random x and y bounded within our pixel array size
     while(len(temp_coords) != k):
-        temp_coords.add(np.random(width-1), np.random(height-1))
+        temp_coords.add((np.random.randint(width-1), np.random.randint(height-1)))
     for x,y in temp_coords:
-        centers.add(pixels[x][y]) #get the RGB/xyz values at that random point in the image
+        # print(pixels)
+        centers.append(img[x][y]) #get the RGB/xyz values at that random point in the image
 
     return centers #we now return k cluster centers that are spawned on random points in our RGB space
     
@@ -43,5 +41,25 @@ def distance(p1, p2): #think of p1 having xyz coords instead of rgb :)
     diff = p1 - p2 #[R1-R2, G1-G2, B1-B2]
     return np.sqrt(np.sum(diff ** 2))
     
-def assign_clusters(img, cluster):
-    for i in range()
+def assign_clusters(img, cluster_centers):
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            pixel = img[x][y]
+
+
+
+img = cv.imread("test_images/1920x1080.jpg", -1) #Read in file as is
+img_height, img_width, _ = img.shape
+
+size = 15
+sigma = 5 #TODO: play around with these numbers to see what works best and not. 5,2 seems to be best
+gaussian = GaussianImage(size, sigma, img) #prob want a bi-soemthing blur too. preserves edges better
+
+num_colors = 128
+print("hi")
+k_means_clustering(100, img)
+
+
+
+            
+            
