aeaa31a8173cd0877fbc8b7fef7db00307ff2b72
areaves1207 <areaves0816@gmail.com>
2025-06-04 22:15:22 -0600

    Color clustering will now produce images with n given colors

--- color_clustering.py
+++ color_clustering.py
@@ -10,16 +10,21 @@ def k_means_clustering(k, img):
     # np.savetxt("my_array.txt", img[0], delimiter=" ", fmt="%d") 
     #centroids holds the center value, clusters holds all of the coords for each pixel and which cluster its assigned
     centroids, clusters = pick_centroids(k, img) #now we have k (x,y,z) values
+    prev_centroids = None
 
     #Assign pixels to clusters
-    for _ in range (2):
-    # while(prev_centroids != centroids):
+    # for _ in range (50):
+    while(True):
         clusters = assign_clusters(img, centroids, clusters)
         centroids = update_centroids(img, clusters)
-        # prev_centroids = centroids
-        # if(np.linalg.norm(np.array(centroids) - np.array(prev_centroids)) <  1e-3):
-        #     print("MINOR CHANGE DETECTED")
-        #     break
+        
+        if prev_centroids is not None:
+            diff = np.linalg.norm(np.array(centroids) - np.array(prev_centroids))  # total Euclidean difference
+            if diff < 1e-4:
+                print("Minor change detected")
+                break
+        prev_centroids = centroids.copy()
+        
     
     clustered_img = np.empty_like(img)    
     for i in range(k):
@@ -61,7 +66,9 @@ def update_centroids(img, clusters):
     for cluster in clusters:
         sum_vector = np.zeros(3, dtype=np.float32)
         for pixel in cluster:
-            sum_vector += img[pixel[0]][pixel[1]].astype(np.float32)
+            x=pixel[0]
+            y=pixel[1]
+            sum_vector += img[x][y].astype(np.float32)
 
         mean = (sum_vector / len(cluster)).astype(np.uint8)
         new_centroids.append(mean)
@@ -79,31 +86,30 @@ def pick_centroids(k, img): #generates k random center points that lie within th
     temp_coords = set() #holds random x and y bounded within our pixel array size
     while(len(temp_coords) != k):
         temp_coords.add((np.random.randint(height-1), np.random.randint(width-1)))
-    for x,y in temp_coords:
-        # print(pixels)
-        centroids.append(img[x][y]) #get the RGB/xyz values at that random point in the image
 
-    # for i in range(k):
-    #     print(centroids[i])
-    #     clusters[i].append(centroids[i])
-
+    centroids = [img[x, y] for x, y in temp_coords] #centroid now olds xyz rgb value 0-255
+    centroids = np.array(centroids, dtype=np.uint8)
     return centroids, clusters #we now return k cluster centers that are spawned on random points in our RGB space
     
         
-def distance(p1, p2): #think of p having xyz coords instead of rgb :)
-    diff = p1 - p2 #[R1-R2, G1-G2, B1-B2] (technically BGR but it doesnt matter here)
-    return np.sqrt(np.sum(diff**2))
+# def distance(p1, p2): #think of p having xyz coords instead of rgb :)
+#     diff = p1 - p2 #[R1-R2, G1-G2, B1-B2] (technically BGR but it doesnt matter here)
+#     return np.sqrt(np.sum(diff**2))
 
 
-img = cv.imread("test_images/1920x1080.jpg", -1) #Read in file as is
+img = cv.imread("test_images/vettriano.jpeg", -1) #Read in file as is
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+img = cv.resize(img, (1920, 1080), interpolation=cv.INTER_AREA)
+
 size = 15
-sigma = 5 #TODO: play around with these numbers to see what works best and not. 5,2 seems to be best
-gaussian = GaussianImage(size, sigma, img) #prob want a bi-soemthing blur too. preserves edges better
+sigma = (5,5) #TODO: play around with these numbers to see what works best and not. 5,2 seems to be best
+gaussian = cv.GaussianBlur(img, sigma, size) #prob want a bi-soemthing blur too. preserves edges better
+bilateral_blurred_img = cv.bilateralFilter(gaussian, 5, 50, 50)
 
 num_colors = 128
 start_time = time.time()
 
-k_means_clustering(12, img)
+k_means_clustering(24, bilateral_blurred_img)
 
 end_time = time.time()
 print(f"Execution time: {end_time - start_time:.4f} seconds")
