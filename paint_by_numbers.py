291feba8b805954896bb8a45be9256269c8f9d76
areaves12 <areaves@mines.edu>
2025-06-06 21:43:33 +0200

    Small modifications + new test img

--- paint_by_numbers.py
+++ paint_by_numbers.py
@@ -3,19 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from color_clustering import k_means_clustering
 import time
+from image_setup import setup_image
 
 
-images = ["1920x1080.jpg", "dali.jpeg", "dog.jpeg", "reef.jpeg", "vettriano.jpeg", "woman_in_hallway.png"]
+images = ["test_images/lemons.jpg", "test_images/1920x1080.jpg", "test_images/dali.jpeg", "test_images/dog.jpeg", "test_images/reef.jpeg", "test_images/vettriano.jpeg", "test_images/woman_in_hallway.png"]
 
-img = cv.imread("test_images/" + images[5], -1) #Read in file as is
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB) #swap from BGR to RGB 
-img = cv.resize(img, (1920, 1080), interpolation=cv.INTER_AREA)
-
-size = 15
-sigma = (5,5)
-gaussian = cv.GaussianBlur(img, sigma, size)
-bilateral_blurred_img = cv.bilateralFilter(gaussian, 5, 50, 50)
+img = setup_image(img_file_location = images[0])
 
 num_colors = 16
-k_means_clustering(num_colors, bilateral_blurred_img)
+k_means_clustering(num_colors, img)
 
