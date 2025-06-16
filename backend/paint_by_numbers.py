0a08146722c15c6abb2d3e4a2ccb6d2a63f8dc0a
areaves12 <areaves@mines.edu>
2025-06-16 20:58:18 +0200

    Palette generation working

--- backend/paint_by_numbers.py
+++ backend/paint_by_numbers.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from palette import draw_palette
+from palette import add_padding, draw_palette_onto_img, generate_palette
 from color_clustering import k_means_clustering
 import time
 from image_utils import display_image, setup_image, display_dual_imgs, combine_images
@@ -11,6 +11,7 @@ from number_drawing import draw_numbers_cv, draw_numbers_pil
 images = ["test_images/sorrento.jpg", "test_images/lemons.jpg", "test_images/1920x1080.jpg", "test_images/dali.jpeg", "test_images/dog.jpeg", "test_images/reef.jpeg", "test_images/vettriano.jpeg", "test_images/woman_in_hallway.png", "test_images/churro.jpg", "test_images/sexy_churro.jpg"]
 
 img = setup_image(img_file_location = "backend/" + images[6], reduce=True)
+height, width, _ = img.shape
 
 num_colors = 16
 #TODO: potential issue i thought of it its possible when we add edges, those pixels are still
@@ -28,5 +29,8 @@ clustered_img, labels, color_pallete, batches, center_of_masses = k_means_cluste
 # final_image_tight = draw_numbers_pil(combined_tight, center_of_masses)
 # display_dual_imgs(final_image, final_image_tight)
 
+padded_img = add_padding(clustered_img, width // 12)
+palette = generate_palette(height, color_pallete)
+final_image = draw_palette_onto_img(padded_img, palette)
 
-display_image(draw_palette(clustered_img, color_pallete))
+display_image(final_image)
--- backend/palette.py
+++ backend/palette.py
@@ -1,22 +1,28 @@
 import cv2 as cv
 import numpy as np
 
-def draw_palette(img, color_palette):
-    font_size = 8
+def generate_palette(img_height, color_palette):
     num_colors = len(color_palette)
-    width, height, _ = img.shape
-    box_size = height // (num_colors*2)
+    box_size = img_height // (num_colors*2 + 1)
 
     #make all white strip of img height
-    palette = np.full(shape=(box_size + 1 + font_size, height, 3), fill_value=[255,255,255], dtype=np.uint8)
+    palette = np.full(shape=(img_height, box_size * 3, 3), fill_value=[255,255,255], dtype=np.uint8)
 
-    for c, color_idx in enumerate(range(0, num_colors)): #loop through each color
-        #the position of the top left of each square for each color:
-        y = c*box_size
+    for c, color_idx in enumerate(range(0, num_colors), start=1): #loop through each color
+        #the position of the top left of each square for each; color:
+        y = c*box_size*2
         top_left_point = (0, y)
         bottom_right_point = (box_size, y + box_size)
         color = tuple(int(c) for c in color_palette[color_idx])
-        cv.rectangle(img = palette, pt1= top_left_point, pt2= bottom_right_point, color=color, thickness=1)
-        cv.putText(img=palette, text=str(color_idx), org=(box_size, (y + box_size)//2), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,0), thickness=2)
+        cv.rectangle(img=palette, pt1=top_left_point, pt2= bottom_right_point, color=color, thickness=-1)
+        cv.putText(img=palette, text=str(color_idx+1), org=(box_size, y + (box_size//2 + 1)), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(0,0,0), thickness=2)
 
     return palette
+
+def draw_palette_onto_img(img, palette):
+    return np.concatenate((img, palette), axis=1)
+
+def add_padding(img, padding_amt, color=[255,255,255]):
+    padding = np.full(shape=(img.shape[0], padding_amt, 3), fill_value=color, dtype=np.uint8)
+    padded_img = np.concatenate((img, padding), axis=1)
+    return padded_img
