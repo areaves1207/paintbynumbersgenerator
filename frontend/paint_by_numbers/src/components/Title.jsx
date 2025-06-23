94d487c5dcfb47aafee104abadd74511014fc10d
areaves1207 <areaves0816@gmail.com>
2025-06-23 00:17:17 -0600

    improved titles

--- frontend/paint_by_numbers/src/components/Title.jsx
+++ frontend/paint_by_numbers/src/components/Title.jsx
@@ -2,8 +2,16 @@ import styles from './title.module.css';
 
 export default function Title(){
     return (
-            <div className={styles.title}>
-                Paint By Numbers Generator
+        <div className={styles.title}>
+            <div className={styles.sirtitle}>
+                PAINT BY NUMBERS GENERATOR
             </div>
+
+            <div className={styles.subtitle}>
+                CREATE ART FROM REALITY
+            </div>
+
+            <button className={styles.button}>MAKE A CANVAS</button>
+        </div>
     );
 }
