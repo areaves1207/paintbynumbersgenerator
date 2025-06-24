f1b47ae15202a76ad97bb71e97efe8c101eccabf
areaves12 <areaves@mines.edu>
2025-06-24 06:58:19 +0200

    Changed file uploading

--- frontend/paint_by_numbers/src/components/Generator.jsx
+++ frontend/paint_by_numbers/src/components/Generator.jsx
@@ -1,11 +1,22 @@
 import styles from "./generator.module.css"
+import { useState } from "react";
 
 export default function Generator(){
+    const [selectedFile, setSelectedFile] = useState(null);
+    
+    const fileUploadHandler = event => {
+        const file = event.target.files[0];
+        console.log(file);
+        setSelectedFile(file);
+    };
+
     return (
         <div className={styles.div}>
             <div className={styles.infoText}>Upload an image to generate it's canvas</div>
 
-            <button className={styles.button}>Upload image</button>
+            <input type='file' accept="image/*" onChange={fileUploadHandler}></input>
+            {/* <input type='file' className={styles.button}>Upload image</input> */}
+            <img src={selectedFile}></img>
         </div>
     );
 }
