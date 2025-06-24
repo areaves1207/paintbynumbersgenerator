8b4ee1923d0065feffc390aa00df84c907eda35c
areaves1207 <areaves0816@gmail.com>
2025-06-23 23:22:57 -0600

    Added image preview

--- frontend/paint_by_numbers/src/components/Generator.jsx
+++ frontend/paint_by_numbers/src/components/Generator.jsx
@@ -3,20 +3,26 @@ import { useState } from "react";
 
 export default function Generator(){
     const [selectedFile, setSelectedFile] = useState(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
     
     const fileUploadHandler = event => {
         const file = event.target.files[0];
         console.log(file);
-        setSelectedFile(file);
+        if(file){
+            setSelectedFile(file);
+            setPreviewUrl(URL.createObjectURL(file));
+        }
+        
     };
 
     return (
         <div className={styles.div}>
             <div className={styles.infoText}>Upload an image to generate it's canvas</div>
 
-            <input type='file' accept="image/*" onChange={fileUploadHandler}></input>
-            {/* <input type='file' className={styles.button}>Upload image</input> */}
-            <img src={selectedFile}></img>
+            <input type='file' accept="image/*" className={styles.button} onChange={fileUploadHandler}></input>
+            {/* <input type='file' >Upload image</input> */}
+
+            <img className={styles.imagePreview} src={previewUrl}></img>
         </div>
     );
 }
