ecab3a061f0de0450ce654f9133338c1986841ad
areaves12 <areaves@mines.edu>
2025-07-03 00:51:18 +0200

    sorry for the spam, i cant figure the issue yet and testing is down

--- backend/process_image.py
+++ backend/process_image.py
@@ -44,35 +44,36 @@ async def create_upload_img(file: UploadFile = File(...), numColors: int = Form(
     print(f"Received NumColors value of: {numColors}")
 
     #read in img from front end as uint8 thru cv
-    contents = await file.read()
-    np_arr = np.frombuffer(contents, np.uint8)
-    img_np = cv.imdecode(np_arr, cv.IMREAD_COLOR)
+    # contents = await file.read()
+    # np_arr = np.frombuffer(contents, np.uint8)
+    # img_np = cv.imdecode(np_arr, cv.IMREAD_COLOR)
 
-    if img_np is None:
-        return{"ERROR": "FAILED TO DECODE IMG"}
+    # if img_np is None:
+    #     return{"ERROR": "FAILED TO DECODE IMG"}
     
-    print(f"Image {file.filename} decoded successfully")
-    result_tight, result_smooth = paint_by_numbers_gen(img_np, numColors)
-    #conv imgs to PIL    
-    result_tight = Image.fromarray(result_tight)
-    result_smooth = Image.fromarray(result_smooth)
+    # print(f"Image {file.filename} decoded successfully")
+    # result_tight, result_smooth = paint_by_numbers_gen(img_np, numColors)
+    # #conv imgs to PIL    
+    # result_tight = Image.fromarray(result_tight)
+    # result_smooth = Image.fromarray(result_smooth)
 
-    zip_buffer = BytesIO()
-    with zipfile.ZipFile(zip_buffer, "w") as zip_file:
-        img1_io = BytesIO()
-        result_tight.save(img1_io, format="PNG")
-        img1_io.seek(0)
-        zip_file.writestr("final_image_tight.png", img1_io.read())
+    # zip_buffer = BytesIO()
+    # with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+    #     img1_io = BytesIO()
+    #     result_tight.save(img1_io, format="PNG")
+    #     img1_io.seek(0)
+    #     zip_file.writestr("final_image_tight.png", img1_io.read())
         
-        img2_io = BytesIO()
-        result_smooth.save(img2_io, format="PNG")
-        img2_io.seek(0)
-        zip_file.writestr("final_image_smooth.png", img2_io.read())
+    #     img2_io = BytesIO()
+    #     result_smooth.save(img2_io, format="PNG")
+    #     img2_io.seek(0)
+    #     zip_file.writestr("final_image_smooth.png", img2_io.read())
     
-    zip_buffer.seek(0)
-    return StreamingResponse(zip_buffer, media_type="application/zip", headers={
-        "Content": "attachment; filename=processed_images.zip"
-    })
+    # zip_buffer.seek(0)
+    # return StreamingResponse(zip_buffer, media_type="application/zip", headers={
+    #     "Content": "attachment; filename=processed_images.zip"
+    # })
+    return "HI"
 
 
 @app.get("/ping")
