fbd98f9b4af6a3e451b68b64497cf911e2bb9130
areaves12 <areaves@mines.edu>
2025-07-03 22:58:08 +0200

    test for backend again

--- backend/process_image.py
+++ backend/process_image.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origins=["https://paintbynumbersgenerator-jet.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
