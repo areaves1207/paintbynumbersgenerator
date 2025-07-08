078f8a38e0a0d8d5acd84b5b77e81da68289b922
areaves12 <areaves@mines.edu>
2025-07-08 03:17:43 +0200

    Made url const

--- frontend/paint_by_numbers/src/components/Generator.jsx
+++ frontend/paint_by_numbers/src/components/Generator.jsx
@@ -55,7 +55,7 @@ const Generator = forwardRef((_, ref) => {
         const formData = new FormData();
         formData.append("file", selectedFile);
         formData.append("numColors", numColors);
-        upload_url = "https://paintbynumbersgenerator-h1lp.onrender.com/upload_img/"
+        const upload_url = "https://paintbynumbersgenerator-h1lp.onrender.com/upload_img/"
         try{
             console.log(`Sending image to url: ${upload_url}`);
 
