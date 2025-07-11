1aefd4080638093456358cf609fbb22e7b481bec
areaves12 <areaves@mines.edu>
2025-07-11 02:06:55 +0200

    Popup now works

--- frontend/paint_by_numbers/src/App.jsx
+++ frontend/paint_by_numbers/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState} from "react";
 import PaintByNumbers from "./components/PaintByNumbers"
 import Header from "./components/Header"
 import './App.css';
@@ -7,10 +7,12 @@ import './components/fonts.css';
 import Gallery from "./components/Gallery";
 import Generator from "./components/Generator";
 import Footer from "./components/Footer";
+import Popup from "./components/Popup";
 
 
 function App() {
   const generatorRef = useRef(null);
+  const [isPopupOpen, setPopupOpen] = useState(true);
 
   const scrollToGenerator = () => {
     generatorRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -20,6 +22,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Popup isOpen={isPopupOpen} onClose={() => setPopupOpen(false)} />
       <div className={StyleSheet.body}>
         <Title onClickScroll={scrollToGenerator}/>
         <Gallery/>
