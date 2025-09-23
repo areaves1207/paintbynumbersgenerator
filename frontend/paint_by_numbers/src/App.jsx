d943d3e14773653a747e81a66f0c60bd23895d34
areaves12 <areaves@mines.edu>
2025-09-23 16:28:23 -0600

    Updated popup message

--- frontend/paint_by_numbers/src/App.jsx
+++ frontend/paint_by_numbers/src/App.jsx
@@ -49,21 +49,25 @@ function App() {
         </h3>
 
         <h4>
+          When I first got this project running, it wasn't that optimized; there were many matrix operations that were very slow. Over the
+          course of a few weeks, I've cut image generation time from ~15 minutes down to ~3! The fun part about this project is that I have implemented just about *everything* myself, such as a custom Canny edge
+          detector, K-Means clustering, palette generation, etc.
+        </h4>
+
+        <h4>
           This site is hosted by Vercel and the back end is hosted by Render. FastAPI is used to connect everything.
           Since both are free hosting sites, the input and output is limited, and so the wait times for generating an
-          image is very long and the outputs are pretty small (640x480).
+          image are unfortunately longer than I'd like and the outputs have to be pretty small (640x480).
         </h4>
 
         <h4>
-          In all honesty too, it is not extremely optimized, which is a partial reason for the wait time. The next major fix
-          is to make the backend more efficient. I have implemented just about everything myself, such as the custom Canny edge
-          detector, K-Means clustering, and a tight edge detector. The good news is I have my implementations working! They just
-          aren't optimized at the moment, unfortunately.
+          Thank you for checking it out! It's been a fun project to work on, and my first ever full stack development!
         </h4>
 
-        <h4>
-          Thank you for checking it out though! It's been a fun project to work on, and my first ever full stack development!
-        </h4>
+        <h6>
+          To see this message again, click the "About" button in the top right corner, or clear your cookies.
+        </h6>
+
       </Popup>}
 
       <div className={StyleSheet.body}>
