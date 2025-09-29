1f0464e8ee72b14c867893cec2a6d27b601e487f
areaves1207 <areaves0816@gmail.com>
2025-09-28 21:59:03 -0600

    Broke final image into canvas and palette

--- backend/paint_by_numbers.py
+++ backend/paint_by_numbers.py
@@ -12,53 +12,42 @@ import time
 
 def paint_by_numbers_gen(img, num_colors, force_scale = True):
     try:
+        total_time_start = time.time()
         img = setup_image(img, force_scale)
         height, width, _ = img.shape
 
         print("k means clustering")
         start_time = time.time()
         clustered_img, labels, color_pallete, batches, center_of_masses = k_means_clustering(num_colors, img)
-        print("K-means clustering finished in: %f seconds", (time.time() - start_time) ) #currently runs in ~140s
+        print("K-means clustering finished in: ", (time.time() - start_time) ) #currently runs in ~140s
 
         print("edge detector")
         start_time = time.time()
         edges = edge_detector.detect_edges_canny(clustered_img.copy())
-        print("Canny edge detector finished in: %f seconds", (time.time() - start_time) )
+        print("Canny edge detector finished in: ", (time.time() - start_time) )
         start_time = time.time()
-        # edges_tight = edge_detector.detect_edges_tight(clustered_img.copy())
-        # print("Tight edge detector finished in: %f seconds", (time.time() - start_time) )
         
 
         print("Combining edges and photo")
         start_time = time.time()
         combined = combine_images(clustered_img.copy(), edges.copy())
-        # combined_tight = combine_images(clustered_img.copy(), edges_tight.copy())
-        # print("Combined edges and photos in: %f seconds", (time.time() - start_time) )
 
         print("Configuring numbers")
         start_time = time.time()
-        numbered_image = draw_numbers_pil(combined, center_of_masses)
-        # numbered_image_tight = draw_numbers_pil(combined_tight, center_of_masses)
-        # print("Numbers configured in: %f seconds", (time.time() - start_time) )
+        canvas = draw_numbers_pil(combined, center_of_masses)
 
-        print("Creating padding, drawing pallete")
+        print("Drawing palete")
         start_time = time.time()
-        padded_img = add_padding(numbered_image, width // 15)
+        padded_img = add_padding(canvas, width // 15)
         palette = generate_palette(height, color_pallete)
-        print("Padding and pallete completed in: %f seconds", (time.time() - start_time) )
+        print("Padding and palete completed in: ", (time.time() - start_time) )
 
-        print("Finalizing image")
+        # print("Finalizing image")
         start_time = time.time()
-        # padded_img_t = add_padding(numbered_image_tight, width // 15)
-        final_image = draw_palette_onto_img(padded_img, palette)
-        # final_image_t = draw_palette_onto_img(padded_img_t, palette)
-        print("Image finalized in: %f seconds", (time.time() - start_time) )
+        combined_image = draw_palette_onto_img(padded_img, palette)
 
-        # display_image(final_image)
-        # display_dual_imgs(final_image_t, final_image)
-        print("Image complete")
-        # return final_image_t, final_image #_t uses accurate line detector, other uses gaussian
-        return final_image
+        print("Image complete. Total generation time: ",  (time.time() - total_time_start))
+        return combined_image, canvas, palette
     except:
         print("Error in processing")
         return None
